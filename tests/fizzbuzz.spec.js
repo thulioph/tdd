d258d73a27428bb2bd026299da4af60adeb85572
Thulio Philipe <thulioph@gmail.com>
2017-11-29 19:15:07 -0300

    Adds a fizzbuzz module and test.

---
+++ tests/fizzbuzz.spec.js
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import FizzBuzz from '../src/fizzbuzz';
+
+describe('FizzBuzz', () => {
+  it('should return `Fizz` when the number is divisible by 3.', () => {
+    expect(FizzBuzz(3)).to.be.equal('Fizz');
+    expect(FizzBuzz(6)).to.be.equal('Fizz');
+  });
+
+  it('should return `Buzz` when the number is divisible by 5.', () => {
+    expect(FizzBuzz(5)).to.be.equal('Buzz');
+  });
+
+  it('should return `FizzBuzz` when the number is divisible by 3 and 5.', () => {
+    expect(FizzBuzz(15)).to.be.equal('FizzBuzz');
+  });
+
+  it('should return `number` when the number is non-divisible.', () => {
+    expect(FizzBuzz(92)).to.be.equal(92);
+  });
+});
