00f63dc545c4a324c79bdf8d65c3f4ed27098b79
Thulio Philipe <thulioph@gmail.com>
2017-11-29 19:02:06 -0300

    Updates the calc file.

--- src/calc.js
+++ src/calc.js
@@ -2,7 +2,11 @@ const sum = (num1, num2) => num1 + num2;
 const sub = (num1, num2) => num1 - num2;
 const mult = (num1, num2) => num1 * num2;
 const div = (num1, num2) => {
-  return (num1 === 0) || (num2 === 0) ? "It's impossible to divide something by zero." : num1 / num2;
+  if (num1 === 0 || num2 === 0) {
+    return "It's impossible to divide something by zero.";
+  }
+
+  return num1 / num2;
 };
 
 export {
